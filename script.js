414fc7f3f8fbdc913aef5a8a83c29e8d76ff477b
streamfoxoficial <gestionapoyoalojamiento@gmail.com>
2026-03-15 23:33:02 -0500

    Create script.js with JavaScript functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript functionality
+
+function showCurrentDateTime() {
+    const currentDateTime = new Date();
+    console.log(`Current Date and Time (UTC): ${currentDateTime.toISOString()}`);
+}
+
+showCurrentDateTime();
